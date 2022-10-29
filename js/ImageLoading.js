9d642229abb5b0231de3cd861064a351d9a07961
Joshua Waheed <joshua.waheed@gmail.com>
2022-10-29 16:42:47 +0100

    Add more tile types and organise images and scripts into folders

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -1,11 +1,14 @@
 var carPic = document.createElement("img");
 var roadPic = document.createElement("img");
 var wallPic = document.createElement("img");
+var goalPic = document.createElement("img");
+var treePic = document.createElement("img");
+var flagPic = document.createElement("img");
 var picsToLoad = 0;
 
 function beginLoadingImage(imgVar, fileName) {
     imgVar.onload = countLoadedImagesAndLaunchIfReady;
-    imgVar.src = fileName;
+    imgVar.src = "images/" + fileName;
 }
 
 function countLoadedImagesAndLaunchIfReady() {
@@ -20,7 +23,10 @@ function loadImages() {
     var imageList = [
         { varName: carPic, theFile: "player1car.png" },
         { varName: roadPic, theFile: "track_road.png" },
-        { varName: wallPic, theFile: "track_wall.png" }
+        { varName: wallPic, theFile: "track_wall.png" },
+        { varName: goalPic, theFile: "track_goal.png" },
+        { varName: treePic, theFile: "track_tree.png" },
+        { varName: flagPic, theFile: "track_flag.png" }
     ];
 
     picsToLoad = imageList.length;
