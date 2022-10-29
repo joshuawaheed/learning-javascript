a47c46e407e5831e4af594c27577898f262fd37f
Joshua Waheed <joshua.waheed@gmail.com>
2022-10-29 17:18:02 +0100

    Improve performance when loading and drawing tiles

--- js/Track.js
+++ js/Track.js
@@ -42,32 +42,22 @@ function carTrackHandling() {
 }
 
 function drawTracks() {
+    var arrayIndex = 0;
+    var drawTileX = 0;
+    var drawTileY = 0;
+    
     for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
         for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
-            var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
             var tileKindHere = trackGrid[arrayIndex];
-            var useImg;
-            
-            switch(tileKindHere) {
-                case TRACK_ROAD:
-                    useImg = roadPic;
-                    break;
-                case TRACK_WALL:
-                    useImg = wallPic;
-                    break;
-                case TRACK_GOAL:
-                    useImg = goalPic;
-                    break;
-                case TRACK_TREE:
-                    useImg = treePic;
-                    break;
-                case TRACK_FLAG:
-                    useImg = flagPic;
-                    break;
-            }
+            var useImg = trackPics[tileKindHere];
+            canvasContext.drawImage(useImg, drawTileX, drawTileY);
 
-            canvasContext.drawImage(useImg, TRACK_W * eachCol, TRACK_H * eachRow);
+            drawTileX += TRACK_W;
+            arrayIndex++;
         }
+
+        drawTileX = 0;
+        drawTileY += TRACK_H;
     }
 }
 
