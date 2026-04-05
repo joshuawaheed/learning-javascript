fd8277837d318c21a2e51bdc4fb479bd86dfaf77
joshuawaheed <joshua.waheed@gmail.com>
2026-04-05 13:19:08 +0100

    Testing basics with Vitest

---
+++ Vitest/basic-testing-nodejs-esmodules/src/util/numbers.test.js
@@ -0,0 +1,22 @@
+import { it, expect } from "vitest";
+
+import { transformToNumber } from "./numbers";
+
+it("should transform a string number to a number of type number", () => {
+    const input = "1";
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeTypeOf("number");
+});
+
+it("should yield NaN for non-transformable values", () => {
+    const input1 = "invalid";
+    const input2 = {};
+
+    const result1 = transformToNumber(input1);
+    const result2 = transformToNumber(input2);
+
+    expect(result1).toBeNaN();
+    expect(result2).toBeNaN();
+});
